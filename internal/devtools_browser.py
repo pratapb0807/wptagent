ae763b8562da44d665f50555969771474dad0021
Bhanu Pratap <bpratap@bpratap-ltm.internal.salesforce.com>
2020-06-25 09:02:20 +0530

    check after removing lh plugin and decreasing max time

--- internal/devtools_browser.py
+++ internal/devtools_browser.py
@@ -588,9 +588,9 @@ class DevtoolsBrowser(object):
                        '--disable-cpu-throttling',
                        '--throttling-method', 'provided',
                        '--enable-error-reporting',
-                       '--max-wait-for-load', str(int(time_limit * 5000)),
+                       '--max-wait-for-load', str(int(time_limit * 1000 * 3)),
                        '--port', str(task['port']),
-                       '--plugins','lighthouse-plugin-greenhouse',
+                    #    '--plugins','lighthouse-plugin-greenhouse',
                        '--output', 'html',
                        '--output', 'json',
                        '--output-path', '"{0}"'.format(output_path)]
