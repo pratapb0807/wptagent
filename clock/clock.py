dca3f9fef1988fe462bee3d58d2660f58d8ca814
Bhanu Pratap <bpratap@bpratap-ltm.internal.salesforce.com>
2020-07-03 15:19:31 +0530

    dms to scraper

--- clock/clock.py
+++ clock/clock.py
@@ -1,11 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
+import os
+from urllib2 import urlopen, Request
+from bs4 import BeautifulSoup as soup
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    res = requests.get("https://nosnch.in/ebc77fbc59")
-    print(res)
+    
 
 sched.start()
